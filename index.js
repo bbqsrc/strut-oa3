a89e4a946cbaf21d5d765b193c1ae08475216504
Brendan Molloy <brendan@bbqsrc.net>
2017-12-31 00:10:55 +0100

    Weeee

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ const yaml = require("js-yaml")
 const jref = require("json-ref-lite")
 const fs = require("fs")
 
+function preprocessTree(tree) {
+  // Make sure all schema items have their key inside them
+  if (tree.components && tree.components.schemas) {
+    Object.keys(tree.components.schemas).forEach(key => {
+      const obj = tree.components.schemas[key]
+
+      obj.key = key
+    })
+  }
+
+  return tree
+}
+
 async function main() {
-  const tree = jref.resolve(yaml.safeLoad(fs.readFileSync("./bahkat-openapi.yaml", "utf8")))
-  console.dir(tree.paths['/index.json'].get.responses[200].content['application/json'].schema)
+  const tree = jref.resolve(preprocessTree(yaml.safeLoad(fs.readFileSync("./bahkat-openapi.yaml", "utf8"))))
+  console.dir(tree.paths['/index.json']
+    .get.responses[200]
+    .content['application/json'].schema)
+  
 }
 
 main()
